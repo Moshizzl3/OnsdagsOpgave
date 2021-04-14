4bf4ff981bc8d9101d0b8b77efee3acac069d8fe
Christoffer <christofferp97@hotmail.com>
2021-04-14 14:32:53 +0200

    Nu der tilføjet klasser, metoder og andet.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,36 @@
+import java.util.ArrayList;
+
 public class Main {
+private ArrayList<Studerende>elevliste = new ArrayList<>();
+  Studerende student = new Studerende("Hans");
+  Studerende student1 = new Studerende("Bo");
+
+
+
 
   public static void main(String[] args) {
+    Main main = new Main();
+
+    main.renderElever();
+    main.visListe();
+
+
+    }
+    public void visListe()  {
+      for (int i = 0; i < elevliste.size(); i++) {
+        System.out.println(elevliste.get(i).getNavn() + ": ");
+        elevliste.get(i).visKarakterer();
+      }
+    }
+
+    public void renderElever(){
+      student.addKarakterer(2,2,2,2,2,2,2);
+      student1.addKarakterer(2,2,2,2,2,2,2);
+
+    }
+
+
 
   }
 
-}
+
--- src/Studerende.java
+++ src/Studerende.java
@@ -1,9 +1,40 @@
+import java.util.ArrayList;
+
 public class Studerende {
 
-  private String name;
+  private String navn;
+  private ArrayList<Integer> karakterer = new ArrayList<>();
 
-  public void uGotAnA(){
-    System.out.println("random metode");
+  public Studerende(String navn) {
+    this.navn = navn;
   }
 
+  public String getNavn() {
+    return navn;
+  }
+
+  public void addKarakterer(int a,int b,int c, int d ,int e, int q, int u) {  //Sende karakterer videre
+    karakterer.add(a);
+    karakterer.add(b);
+    karakterer.add(c);
+    karakterer.add(d);
+    karakterer.add(e);
+    karakterer.add(q);
+    karakterer.add(u);
+  }
+  public void visKarakterer() {
+    for (int i = 0; i < karakterer.size(); i++) {
+      System.out.print(karakterer.get(i) + " ");
+    }
+    System.out.println();
+  }
+
+  public double vægtetKarakter(int karakter1, int karakter2){
+    double vægtetKarakter = 0;
+    vægtetKarakter += (karakter1*0.25);
+    vægtetKarakter += (karakter2 *0.75);
+
+    return vægtetKarakter;
+
+  }
 }
