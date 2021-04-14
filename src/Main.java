c94be7fbb414322964a62bf6b829b854d1d5cd19
Christoffer <christofferp97@hotmail.com>
2021-04-14 13:07:29 +0200

    Dette må være test 3! Fra intellij

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
 
   public static void main(String[] args) {
     System.out.println("Hej Christoffer, virker det?");
+    System.out.println("10-4 det er forstået og virker");
   }
+
 }
